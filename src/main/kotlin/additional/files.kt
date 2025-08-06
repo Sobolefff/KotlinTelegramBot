6954f5aa44480461929f02e9c06c7d1dd988547b
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2025-08-06 12:19:23 +0300

    KTB-02 реализовал функционал статистики

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -3,6 +3,7 @@ package org.example.additional
 import java.io.File
 
 const val DICTIONARY_FILE_PATH = "words.txt"
+const val PERCENT_SCALE = 100
 
 data class Word(
     val original: String,
@@ -18,16 +19,30 @@ fun main() {
         println("Меню:")
         println(
             """
+            ---------------
             1 - Учить слова
             2 - Статистика
             0 - Выход
+            ---------------
+            
         """.trimIndent()
         )
+        print("Введите номер пункта меню: ")
         when (readlnOrNull()?.toInt()) {
             1 -> println("Вы выбрали пункт \"учить слова\"")
-            2 -> println("Вы выбрали пункт \"статистика\"")
+            2 -> {
+                	println("Вы выбрали пункт \"статистика\"")
+                	val learnedWordsCount = dictionary.filter {
+                    	it.correctAnswersCount >= 3
+                	}.size
+                    val totalWords = dictionary.size
+                    if (!dictionary.isEmpty()) {
+                    	val percentLearnedWords = (learnedWordsCount.toDouble() / dictionary.size * PERCENT_SCALE).toInt()
+                    	println("Выучено $learnedWordsCount из $totalWords слов | $percentLearnedWords%\n")
+                    } else println("Словарь пустой\n")    
+                }
             0 -> return
-            else -> println("Неверный пункт меню, введите 1, 2 или 0")
+            else -> println("Неверный пункт меню, введите 1, 2 или 0\n")
         }
     }
 
